1638ea2f10d67fe822b529969a5b89d682588f5c
pawcharz <pawel.blicharz.dev@gmail.com>
2024-12-17 09:48:37 +0100

    Add downscaled alexnet model

--- src/models/custom.py
+++ src/models/custom.py
@@ -8,6 +8,76 @@ class CustomModel(nn.Module):
     def __init__(self, num_classes: int = 5, dropout: float = 0.5) -> None:
         super().__init__()
         
+        BASE_SIZE = 16
+        
+        # Images
+        self.images_features = nn.Sequential(
+            nn.Conv2d(3, BASE_SIZE, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(BASE_SIZE, BASE_SIZE*3, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(BASE_SIZE*3, BASE_SIZE*6, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(BASE_SIZE*6, BASE_SIZE*4, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(BASE_SIZE*4, BASE_SIZE*4, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.images_avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.images_classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(BASE_SIZE*4 * 6 * 6, BASE_SIZE*8),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(BASE_SIZE*8, BASE_SIZE*4),
+            nn.ReLU(inplace=True),
+            nn.Linear(BASE_SIZE*4, num_classes),
+        )
+        
+        # Edges
+        # self.edges_features = nn.Sequential(
+        #     nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        #     nn.Conv2d(64, 192, kernel_size=5, padding=2),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        #     nn.Conv2d(192, 384, kernel_size=3, padding=1),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(384, 256, kernel_size=3, padding=1),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(256, 256, kernel_size=3, padding=1),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        # )
+        # self.edges_avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        # self.edges_classifier = nn.Sequential(
+        #     nn.Dropout(p=dropout),
+        #     nn.Linear(256 * 6 * 6, 4096),
+        #     nn.ReLU(inplace=True),
+        #     nn.Dropout(p=dropout),
+        #     nn.Linear(4096, 4096),
+        #     nn.ReLU(inplace=True),
+        #     nn.Linear(4096, num_classes),
+        # )
+
+    def forward(self, image: torch.Tensor, edges: torch.Tensor) -> torch.Tensor:
+        
+        images = self.images_features(image)
+        images = self.images_avgpool(images)
+        print(images.shape)
+        images = self.images_classifier(images)
+        
+        return images
+      
+      
+class CustomModel_Old(nn.Module):
+    def __init__(self, num_classes: int = 5, dropout: float = 0.5) -> None:
+        super().__init__()
+        
         weights = ResNet18_Weights.DEFAULT
         self.resnet18 = resnet18(weights=weights, progress=False)
         
